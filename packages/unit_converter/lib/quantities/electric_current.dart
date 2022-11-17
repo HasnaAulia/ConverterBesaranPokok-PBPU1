90a8d190a36705113608ec8156712dbc0132ed3b
AmalliaPutriA <105690914+AmalliaPutriA@users.noreply.github.com>
2022-11-17 15:36:54 +0700

    Electric Currency

--- packages/unit_converter/lib/quantities/electric_current.dart
+++ packages/unit_converter/lib/quantities/electric_current.dart
@@ -1,20 +1,52 @@
 import '../conversion/conversion_node.dart';
 import '../conversion/conversion_tree.dart';
+import '../unit_converter.dart';
 import '../units/electric_current_unit.dart';
 import '../units/unit.dart';
 import 'base_quantity.dart';
+import 'dart:math';
 
 class ElectricCurrent extends BaseQuantity<ElectricCurrent> {
-  ElectricCurrent(num value, Unit<ElectricCurrent> unit)
-      : super(
-          value: value,
-          unit: unit,
-        );
+  static final ConversionTree<ElectricCurrent>_tree = ConversionTree<ElectricCurrent>(
+    data: ConversionNode<ElectricCurrent>(unit : ElectricCurrentUnit.ampere, children: [
+      ConversionNode<ElectricCurrent>(
+        unit: ElectricCurrentUnit.kiloampere,
+        coefficientProduct: pow(10,-3),
+      ),
+      ConversionNode<ElectricCurrent>(
+        unit:ElectricCurrentUnit.miliampere,
+        coefficientProduct: pow(10, 3)
+      ),
+      ConversionNode<ElectricCurrent>(
+        unit:ElectricCurrentUnit.statampere,
+        coefficientProduct: 2997924536.8,
+      ),
+      ConversionNode<ElectricCurrent>(
+        unit:ElectricCurrentUnit.miliampere,
+        coefficientProduct: pow(10, -1)
+      ),
+    ]));
+
+    ElectricCurrent(num value, Unit<ElectricCurrent> unit)
+    : super(
+      value: value,
+      unit: unit,
+    );
 
   @override
   num convertTo(
-    Unit<ElectricCurrent> to,
+    Unit<ElectricCurrent> to, 
   ) {
-    return value;
+    return Conversion<ElectricCurrent>(_tree).convert(super.value, super.unit, to);
+  }
+
+  static List<String> getAllUnits(){
+    List<ConversionNode<ElectricCurrent>> nodes = _tree.data.getTreeAsList();
+    List<String> units = [];
+
+    for (ConversionNode<ElectricCurrent> node in nodes){
+      units.add(node.unit.symbol);
+    }
+    return units;
   }
 }
