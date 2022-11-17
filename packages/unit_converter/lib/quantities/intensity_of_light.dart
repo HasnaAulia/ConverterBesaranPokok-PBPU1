0191a9937fc3722bbff734693fdc9a881eb9037f
christinexxx1 <christine.sutanto0901@gmail.com>
2022-11-17 16:45:07 +0700

    intensity of light

---
+++ packages/unit_converter/lib/quantities/intensity_of_light.dart
@@ -0,0 +1,49 @@
+import 'dart:math';
+
+import 'package:unit_converter/unit_converter.dart';
+
+import '../conversion/conversion_node.dart';
+import '../conversion/conversion_tree.dart';
+import '../units/intensity_of_light_unit.dart';
+import '../units/unit.dart';
+import 'base_quantity.dart';
+
+class IntensityOfLight extends BaseQuantity<IntensityOfLight> {
+  static final ConversionTree<IntensityOfLight> _tree = ConversionTree(
+    data: ConversionNode<IntensityOfLight>(
+      unit: IntensityOfLightUnit.candela,
+      children: [
+        ConversionNode<IntensityOfLight>(
+          unit: IntensityOfLightUnit.candelagerman,
+          coefficientProduct: 95/100,
+        ),
+        ConversionNode<IntensityOfLight>(
+          unit: IntensityOfLightUnit.candelauk,
+          coefficientProduct: 90/100,
+        ),
+      ],  
+  ),
+  );
+
+  @override
+  num convertTo(
+    Unit<IntensityOfLight> to,
+  ) {
+    return Conversion<IntensityOfLight>(_tree).convert(super.value, super.unit, to);
+  }
+
+  static List<String> getAllUnits() {
+    List<ConversionNode<IntensityOfLight>> nodes = _tree.data.getTreeAsList();
+    List<String> units = [];
+
+    for (ConversionNode<IntensityOfLight> node in nodes) {
+      units.add(node.unit.symbol);
+    }
+    return units;
+  }
+}
+
+
+
+
+
