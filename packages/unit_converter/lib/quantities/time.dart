611740773c870dc5f1c0beb4879dad9955eec6d8
Syauqizu <zulfanaziz55@student.ub.ac.id>
2022-11-29 11:53:26 +0700

    Time

--- packages/unit_converter/lib/quantities/time.dart
+++ packages/unit_converter/lib/quantities/time.dart
@@ -1,9 +1,24 @@
 import 'package:unit_converter/unit_converter.dart';
 
+import '../conversion/conversion.dart';
+import '../conversion/conversion_node.dart';
+import '../conversion/conversion_tree.dart';
+import '../units/time_unit.dart';
 import '../units/unit.dart';
 import 'base_quantity.dart';
 
 class Time extends BaseQuantity<Time> {
+  static final ConversionTree<Time> _tree = ConversionTree<Time>(
+    data: ConversionNode<Time>(
+      unit: TimeUnit.second,
+      children: [
+        ConversionNode<Time>(
+          unit: TimeUnit.minute,
+          coefficientProduct: 1 / 60,
+        ),
+      ],
+    ),
+  );
   Time(num value, Unit<Time> unit)
       : super(
           value: value,
@@ -14,6 +29,16 @@ class Time extends BaseQuantity<Time> {
   num convertTo(
     Unit<Time> to,
   ) {
-    return value;
+    return Conversion<Time>(_tree).convert(super.value, super.unit, to);
+  }
+
+  static List<String> getAllUnits() {
+    List<ConversionNode<Time>> nodes = _tree.data.getTreeAsList();
+    List<String> units = [];
+
+    for (ConversionNode<Length> node in nodes) {
+      units.add(node.unit.symbol);
+    }
+    return units;
   }
 }
